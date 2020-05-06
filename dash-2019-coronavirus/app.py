21d2111092a4256cc28c36aa08c3afdd07b9f20f
JUN YE <perishleaf@gmail.com>
2020-05-06 22:16:30 +1000

    daily update

--- dash-2019-coronavirus/app.py
+++ dash-2019-coronavirus/app.py
@@ -1654,7 +1654,7 @@ app.layout = html.Div(
                     children=[
                         html.A(
                             'COVID-19 infographic in Australia', 
-                            href='https://www.health.gov.au/sites/default/files/documents/2020/05/coronavirus-covid-19-at-a-glance-coronavirus-covid-19-at-a-glance-infographic_2.pdf', 
+                            href='https://www.health.gov.au/sites/default/files/documents/2020/05/coronavirus-covid-19-at-a-glance-coronavirus-covid-19-at-a-glance-infographic_4.pdf', 
                             target='_blank'
                                 ),
                     ],
